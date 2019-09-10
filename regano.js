c5574ddafe80227e17a5994d69cc99fc7ae0f735
alexdelamora <creckwfs@gmail.com>
2019-09-10 18:44:16 -0500

    Ejercicio reganño

---
+++ regano.js
@@ -0,0 +1,5 @@
+let num1 = parseFloat(prompt("¿Desde que numero quieres que empiece"))
+let num2 = parseFloat(prompt("Hasta que numero quieres que termine"))
+for (num1; num1 <= num2; num1 ++) {
+    console.log(num1)
+}
